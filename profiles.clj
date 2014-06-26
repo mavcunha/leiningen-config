07eccc0915487eb8b1d05dee7091744e2403092f
Marco Valtas <mavcunha@gmail.com>
2014-06-26 08:04:14 -0300

    Duh, wrong name for jvm options

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]
- :jvm-options ["-Dapple.awt.UIElement=true"]}}
+ {:plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]] ; a REPL interface that can be used with vim-fireplace
+ :jvm-opts ["-Dapple.awt.UIElement=true"]}} ; do not open an window when starting a REPL
