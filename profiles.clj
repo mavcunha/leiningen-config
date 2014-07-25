c07b06e370ffa68d1f06cfc0b8def57f9c4355e6
Marco Valtas <mavcunha@gmail.com>
2014-07-25 07:32:50 -0300

    Use Clojure on shell onliners and scripts

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,6 @@
 {:user
  {:jvm-opts ["-Dapple.awt.UIElement=true"]          ; this doesn't work, LEIN_JVM_OPTS env variable works though.
  :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]     ; repl for vim-fireplace
-           [lein-pprint "1.1.1"]]}}                 ; pretty prints stuff, like 'lein with-profile user pprint'
+           [lein-pprint "1.1.1"]                    ; pretty prints stuff, like 'lein with-profile user pprint'
+           [lein-exec "0.3.4"]                      ; execute clojure programs as shell scripting
+           ]}}
